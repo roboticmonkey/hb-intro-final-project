6ea777af51c9f4364ab779f76374a4bc467cf1bd
aiden ward <aiden@Lagertha.local>
2015-10-02 14:03:50 -0700

    working version

--- battleship_driver.py
+++ battleship_driver.py
@@ -1,5 +1,8 @@
 import board
 
-player_shipboard = board.Board()
+player_shipgrid = board.Board()
+player_bombgrid = board.Board()
 print "Player Ships"
-player_shipboard.draw_board_loop(player_shipboard.board)
+player_shipgrid.draw_board_loop(player_shipgrid.board)
+print "Player Bomb Board"
+player_bombgrid.draw_board_loop(player_bombgrid.board)
