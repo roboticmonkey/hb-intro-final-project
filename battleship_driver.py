c171b0e24cb33685ede546432a50a91515ae0db8
aiden ward <aiden@Lagertha.local>
2015-10-03 11:25:19 -0700

    experimenting with ship class

--- battleship_driver.py
+++ battleship_driver.py
@@ -1,4 +1,6 @@
 import board
+import ship
+
 
 player_shipgrid = board.Board()
 player_bombgrid = board.Board()
@@ -6,3 +8,12 @@ print "Player Ships"
 player_shipgrid.draw_board_loop(player_shipgrid.board)
 print "Player Bomb Board"
 player_bombgrid.draw_board_loop(player_bombgrid.board)
+
+carrier = ship.Ship(5, "Carrier")
+print carrier
+print carrier.sunk
+print carrier.end_location
+print type(carrier.size)
+print carrier.boat
+print type(carrier.boat[3])
+print len(carrier.boat)
