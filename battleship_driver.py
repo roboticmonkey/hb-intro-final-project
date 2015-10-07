d13e4a0a5e0cd1f17a4e473a9c24a13c5e431d0d
aiden ward <aiden@Lagertha.local>
2015-10-07 12:42:58 -0700

    working placed ships on board

--- battleship_driver.py
+++ battleship_driver.py
@@ -13,57 +13,55 @@ player_bomb_grid.draw_grid_loop(player_bomb_grid.grid)
 computer = game_pieces.Game_Pieces("Computer")
 
 #testing how to place ships and keep track of location
-#testing horizontal placement
-computer.sub.start_location = "A1"
-computer.sub.end_location = "A3"
-print computer.sub.start_location
-print computer.sub.size
 
-computer.sub.grid_loc_start = [
-			computer.convert_loc_letter_index(player_ship_grid, computer.sub.start_location),
-			computer.convert_loc_str_index(computer.sub.start_location)]
-print computer.sub.grid_loc_start
+print "testing ship placement"
+#hardcoding ship locations
+computer.carrier.start_location ="D8"
+computer.carrier.end_location = "H8"
 
-computer.sub.grid_loc_end = [
-			computer.convert_loc_letter_index(player_ship_grid, computer.sub.end_location),
-			computer.convert_loc_str_index(computer.sub.end_location)]
-print computer.sub.grid_loc_end
+computer.battleship.start_location = "I3"
+computer.battleship.end_location ="I6"
 
-if (computer.sub.grid_loc_start[0] == computer.sub.grid_loc_end[0]):
-	print "Horizontal"
-else: 
-	print "verticle"
+computer.cruiser.start_location = "A7"
+computer.cruiser.end_location = "A9"
 
-player_ship_grid.update_grid_ship_hor(player_ship_grid.grid, computer.sub)
+computer.sub.start_location = "B1"
+computer.sub.end_location = "B3"
+
+computer.destroyer.start_location ="E4"
+computer.destroyer.end_location = "F4"
+#creating the gird locations
+computer.create_ship_grid_locations(player_ship_grid)
+
+#updateing the ship board
+player_ship_grid.update_grid_ship(computer.carrier)
+player_ship_grid.update_grid_ship(computer.destroyer)
+player_ship_grid.update_grid_ship(computer.battleship)
+player_ship_grid.update_grid_ship(computer.sub)
+player_ship_grid.update_grid_ship(computer.cruiser)
+
+#printing an updated board
 print "Player Ship Grid"
 player_ship_grid.draw_grid_loop(player_ship_grid.grid)
 
-#testing vertical placement
-computer.cruiser.start_location = "H1"
-computer.cruiser.end_location = "J1"
-print computer.cruiser.start_location
-print computer.cruiser.size
 
-computer.cruiser.grid_loc_start = [
-			computer.convert_loc_letter_index(player_ship_grid, computer.cruiser.start_location),
-			computer.convert_loc_str_index(computer.cruiser.start_location)]
-print computer.cruiser.grid_loc_start
 
-computer.cruiser.grid_loc_end = [
-			computer.convert_loc_letter_index(player_ship_grid, computer.cruiser.end_location),
-			computer.convert_loc_str_index(computer.cruiser.end_location)]
-print computer.cruiser.grid_loc_end
 
-player_ship_grid.update_grid_ship_vert(player_ship_grid.grid, computer.cruiser)
-print "Player Ship Grid"
-player_ship_grid.draw_grid_loop(player_ship_grid.grid)
 
-print computer.sub.boat
-computer.sub.boat[0]= "X" 
-player_ship_grid.grid[1][1] = "X"
-print computer.sub.boat
 
-print "Player Ship Grid"
-player_ship_grid.draw_grid_loop(player_ship_grid.grid)
+
+
+
+
+
+# print computer.sub.boat
+# computer.sub.boat[0]= "X" 
+# player_ship_grid.grid[1][1] = "X"
+# print computer.sub.boat
+
+# print "Player Ship Grid"
+# player_ship_grid.draw_grid_loop(player_ship_grid.grid)
+
+
 
 
