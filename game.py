5e8558d7a5501374f537fab7e250718e86d50ddd
aiden ward <aiden@Lagertha.local>
2015-10-14 10:11:09 -0700

    working on bomb placement functionality

--- game.py
+++ game.py
@@ -6,262 +6,171 @@ import player
 
 
 print "Welcome to Battleship!"
+#**************************
+#GAME SETUP
 
-#Game set up. Get player 1 name and validate input
-raw_data = raw_input("Player 1 please enter your name. ")
-raw_data = utilities.quick_fix(raw_data)
+#***************************
+#Create player1 player_object
+player1 = player.Player("aiden")
 
-#Validate name
-while (not raw_data.isalpha()):
-	raw_data = raw_input("Please enter a valid name. ")
-	raw_data = utilities.quick_fix(raw_data)
+#Hardcode player1 ship locations
+player1.ships.carrier.grid_loc_start = [3,4]
+player1.ships.carrier.grid_loc_end = [3,8]
+player1.ships.carrier.create_grid_loc_list()
+player1.ships.add_ship_loc_dict(player1.ships.carrier)
+player1.my_ships_board.update_grid_ship(player1.ships.carrier)
 
-#Create player1 object
-player1 = player.Player(raw_data)
-print "Hello,", player1.name
-print "You have 5 ships at varying lengths. You may place your ships horizontally or vertically."
-print "Your 5 ships are:"
+player1.ships.battleship.grid_loc_start = [6,3]
+player1.ships.battleship.grid_loc_end = [9,3]
+player1.ships.battleship.create_grid_loc_list()
+player1.ships.add_ship_loc_dict(player1.ships.battleship)
+player1.my_ships_board.update_grid_ship(player1.ships.battleship)
 
-#List all the ships for the player
-player1.list_ship_name_and_sizes()
+player1.ships.cruiser.grid_loc_start = [5,8]
+player1.ships.cruiser.grid_loc_end = [5,10]
+player1.ships.cruiser.create_grid_loc_list()
+player1.ships.add_ship_loc_dict(player1.ships.cruiser)
+player1.my_ships_board.update_grid_ship(player1.ships.cruiser)
 
-#Print empty ships board
+player1.ships.sub.grid_loc_start = [8,7]
+player1.ships.sub.grid_loc_end = [10,7]
+player1.ships.sub.create_grid_loc_list()
+player1.ships.add_ship_loc_dict(player1.ships.sub)
+player1.my_ships_board.update_grid_ship(player1.ships.sub)
+
+player1.ships.destroyer.grid_loc_start = [2,3]
+player1.ships.destroyer.grid_loc_end = [2,4]
+player1.ships.destroyer.create_grid_loc_list()
+player1.ships.add_ship_loc_dict(player1.ships.destroyer)
+player1.my_ships_board.update_grid_ship(player1.ships.destroyer)
+
+#Print updated players ship board
+print player1.name
 player1.print_my_ships_board()
 
-#FIRST TIME THROUGH
+#Create computer player_object
+computer = player.Player("computer")
+#Hardcode computer ship locations
+computer.ships.carrier.grid_loc_start = [1,3]
+computer.ships.carrier.grid_loc_end = [5,3]
+computer.ships.carrier.create_grid_loc_list()
+computer.ships.add_ship_loc_dict(computer.ships.carrier)
+computer.my_ships_board.update_grid_ship(computer.ships.carrier)
 
-#Ask user for start coordinate
-print "Use letters A-J and numbers 1-10 for coordinates in the format 'A1'. "
-print "Let's place your", player1.ships.carrier.ship_name, "."
-print "Remember, the ", player1.ships.carrier.ship_name, "is ", player1.ships.carrier.size, "cells long."
-raw_data = raw_input("Please give the starting coordinates. Ex. 'A1'. ")
-raw_data = utilities.quick_fix(raw_data)
+computer.ships.battleship.grid_loc_start = [3,5]
+computer.ships.battleship.grid_loc_end = [3,8]
+computer.ships.battleship.create_grid_loc_list()
+computer.ships.add_ship_loc_dict(computer.ships.battleship)
+computer.my_ships_board.update_grid_ship(computer.ships.battleship)
 
-#If not valid coordinate format, re-ask for input
-while (not utilities.validate_user_input(raw_data, player1.my_ships_board)):
-	raw_data = raw_input("Please re-enter coordinates in format 'A1'. ")
-	raw_data = utilities.quick_fix(raw_data)
+computer.ships.cruiser.grid_loc_start = [7,8]
+computer.ships.cruiser.grid_loc_end = [9,8]
+computer.ships.cruiser.create_grid_loc_list()
+computer.ships.add_ship_loc_dict(computer.ships.cruiser)
+computer.my_ships_board.update_grid_ship(computer.ships.cruiser)
 
+computer.ships.sub.grid_loc_start = [8,2]
+computer.ships.sub.grid_loc_end = [8,4]
+computer.ships.sub.create_grid_loc_list()
+computer.ships.add_ship_loc_dict(computer.ships.sub)
+computer.my_ships_board.update_grid_ship(computer.ships.sub)
 
-#Save start location string into ships start_location attribute
-player1.ships.carrier.ship_location_start(raw_data)
-# print player1.ships.carrier.start_location
+computer.ships.destroyer.grid_loc_start = [5,9]
+computer.ships.destroyer.grid_loc_end = [5,10]
+computer.ships.destroyer.create_grid_loc_list()
+computer.ships.add_ship_loc_dict(computer.ships.destroyer)
+computer.my_ships_board.update_grid_ship(computer.ships.destroyer)
 
-#Convert start_location to a grid_location and save 
-player1.ships.carrier.create_grid_start_loc(player1.my_ships_board)
-# print player1.ships.carrier.grid_loc_start
-
-#Check if all_ship dict is empty
-#If all_ship dict is empty get end coordinates
-if (player1.ships.all_ships == {}):
-	#Ask for end location coordinates
-	print player1.ships.carrier.start_location, "is your starting location. Ships may only be horizontal or vertical."
-	raw_data2 = raw_input("Enter your ending coordinates. ")
-	raw_data2 = utilities.quick_fix(raw_data2)
-
-	#If not valid coordinate format, re-ask for input
-	while (not utilities.validate_user_input(raw_data, player1.my_ships_board)):
-		raw_data = raw_input("Please re-enter coordinates in format 'A1'. ")
-		raw_data = utilities.quick_fix(raw_data)
-
-	#Save end location string into ship end_location attribute
-	player1.ships.carrier.ship_location_end(raw_data2)
-	player1.ships.carrier.create_grid_end_loc(player1.my_ships_board)
-	#print player1.ships.carrier.grid_loc_end
-	
-	#Create ship grid location list
-	#If true, add ships grid_loc_list to game_pieces all_ships dict
-	if (player1.ships.carrier.create_grid_loc_list(player1.my_ships_board) == True):
-		
-		#print "list made man"
-		player1.ships.add_ship_loc_dict(player1.ships.carrier)
-
-		#print player1.ships.all_ships
-		
-		#update players ship_board
-		player1.my_ships_board.update_grid_ship(player1.ships.carrier)
-		
-		#Print updated players ship board
-		player1.print_my_ships_board()
-
-	else:
-		#If create_grid_loc_list returns false ask user to re-enter coordinates
-		print "whoops"
-
-#Repeat for each ship
-#SECOND TIME THROUGH
-
-#Ask for user input start coordinates
-print "Let's place your", player1.ships.battleship.ship_name, "."
-print "Remember, the ", player1.ships.battleship.ship_name, "is ", player1.ships.battleship.size, "cells long."
-raw_data = raw_input("Please give the starting coordinates. Ex. 'A1'. ")
-raw_data = utilities.quick_fix(raw_data)
-
-#If not valid coordinate format, re-ask for input
-while (not utilities.validate_user_input(raw_data, player1.my_ships_board)):
-	raw_data = raw_input("Please re-enter coordinates in format 'A1'. ")
-	raw_data = utilities.quick_fix(raw_data)
-
-#Is valid save locaction string
-player1.ships.battleship.ship_location_start(raw_data)
-
-#Convert location string to grid coordinates
-player1.ships.battleship.create_grid_start_loc(player1.my_ships_board)
-
-#Check if ship already at location
-while (player1.ships.is_taken(player1.ships.battleship.grid_loc_start) == True):
-	#If ship already there ask for new location
-	#Erase start locations
-	player1.ships.battleship.erase_start_locations()
-	# print raw_data, "before new input"
-	raw_data = raw_input("Two ships cannot occupy the same spot. Please choose a new spot. ") 
-	# print raw_data, "after new input"
-	raw_data = utilities.quick_fix(raw_data)
-
-	#If not valid coordinate format, re-ask for input
-	while (not utilities.validate_user_input(raw_data, player1.my_ships_board)):
-		raw_data = raw_input("Please re-enter coordinates in format 'A1'. ")
-		raw_data = utilities.quick_fix(raw_data)
-
-	#Is valid save locaction string
-	player1.ships.battleship.ship_location_start(raw_data)
-
-	#Convert location string to grid coordinates
-	player1.ships.battleship.create_grid_start_loc(player1.my_ships_board)
+#Print updated players ship board
+print computer.name
+computer.print_my_ships_board()
 
 
-#If not ask for end location
-print player1.ships.battleship.start_location, "is your starting location. Ships may only be horizontal or vertical."
-raw_data2 = raw_input("Enter your ending coordinates. ")
-raw_data2 = utilities.quick_fix(raw_data2)
+#***********************************
 
-#If not valid coordinate format, re-ask for input
-while (not utilities.validate_user_input(raw_data, player1.my_ships_board)):
-	raw_data = raw_input("Please re-enter coordinates in format 'A1'. ")
-	raw_data = utilities.quick_fix(raw_data)
+#START CODE
 
-#If valid save end location string into ship end_location attribute
-player1.ships.battleship.ship_location_end(raw_data2)
-player1.ships.battleship.create_grid_end_loc(player1.my_ships_board)
-# print player1.ships.battleship.grid_loc_start[0], ":location start"
-# print player1.ships.battleship.create_grid_loc_list(player1.my_ships_board)
-# print player1.ships.battleship.grid_loc_end[0], ":location end"
-# print player1.ships.battleship.grid_loc_list, ":location list"
+# Ask for string bomb location
 
-# Create grid location list
-# print "starting the check of ship list.\n"
-if (player1.ships.battleship.create_grid_loc_list(player1.my_ships_board) == True):
-	
-	#Check if grid location list matches any already placed ship
-	if (player1.ships.is_overlap(player1.ships.battleship) == True):
-		
-		#If True, give error and ask for new start location
-		#Set all location values to empty
-		print "Ships cannot overlap. Re-enter ship location.\n"
-		
-		#Erase start locations
-		player1.ships.battleship.erase_start_locations()
-	
-		#Erase end locations
-		player1.ships.battleship.erase_end_locations()
-				
-		#Erase grid location list
-		player1.ships.battleship.erase_grid_location_list()
-		# print "all ship location variables should be empty"
-		# print "start_location:", player1.ships.battleship.start_location
-		# print "end location:", player1.ships.battleship.end_location
-		# print "grid location start:", player1.ships.battleship.grid_loc_start
-		# print "grid location end:", player1.ships.battleship.grid_loc_end
-		# print "grid location list:", player1.ships.battleship.grid_loc_list
-	
-	#START OVER FOR INFORMATION
+# Validate string bomb location
 
-		print "Let's place your", player1.ships.battleship.ship_name, "."
-		print "Remember, the ", player1.ships.battleship.ship_name, "is ", player1.ships.battleship.size, "cells long."
-		raw_data = raw_input("Please give the starting coordinates. Ex. 'A1'. ")
-		raw_data = utilities.quick_fix(raw_data)
+# If invalid ask for string bomb location again
 
-		#If not valid coordinate format, re-ask for input
-		while (not utilities.validate_user_input(raw_data, player1.my_ships_board)):
-			raw_data = raw_input("Please re-enter coordinates in format 'A1'. ")
-			raw_data = utilities.quick_fix(raw_data)
+# If valid save string
 
-		#Is valid save locaction string
-		player1.ships.battleship.ship_location_start(raw_data)
+# Convert string location to a grid location
 
-		#Convert location string to grid coordinates
-		player1.ships.battleship.create_grid_start_loc(player1.my_ships_board)
-		#Check if ship already at location
-		while (player1.ships.is_taken(player1.ships.battleship.grid_loc_start) == True):
-			
-			#If ship already there ask for new location
-			# print raw_data, "before new input"
-			raw_data = raw_input("Two ships cannot occupy the same spot. Please choose a new spot. ") 
-			# print raw_data, "after new input"
-			raw_data = utilities.quick_fix(raw_data)
+# Check if bomb location is in bomb_list
 
-			#If not valid coordinate format, re-ask for input
-			while (not utilities.validate_user_input(raw_data, player1.my_ships_board)):
-				raw_data = raw_input("Please re-enter coordinates in format 'A1'. ")
-				raw_data = utilities.quick_fix(raw_data)
+# 	If TRUE, 
+# 		Print ERROR MSG: already used pick new location
 
-			#Is valid save locaction string
-			player1.ships.battleship.ship_location_start(raw_data)
+# 		CODE ASK FOR NEW LOCATION
 
-			#Convert location string to grid coordinates
-			player1.ships.battleship.create_grid_start_loc(player1.my_ships_board)
+# 	If FALSE in bomb_list, 
 
+# 		Check if opponents ship @ location
 
-		#If not ask for end location
-		print player1.ships.battleship.start_location, "is your starting location. Ships may only be horizontal or vertical."
-		raw_data2 = raw_input("Enter your ending coordinates. ")
-		raw_data2 = utilities.quick_fix(raw_data2)
+# 			If FALSE
 
-		#If not valid coordinate format, re-ask for input
-		while (not utilities.validate_user_input(raw_data, player1.my_ships_board)):
-			raw_data = raw_input("Please re-enter coordinates in format 'A1'. ")
-			raw_data = utilities.quick_fix(raw_data)
+# 				Save bomb location to bomb_list
 
-		#If valid save end location string into ship end_location attribute
-		player1.ships.battleship.ship_location_end(raw_data2)
-		player1.ships.battleship.create_grid_end_loc(player1.my_ships_board)
+# 				Update player enemy ship board with MISS
 
-		# Create grid location list
-		if (player1.ships.battleship.create_grid_loc_list(player1.my_ships_board) == True):
+# 				Update opponents  my ship board with MISS
 
-			#Check if grid location list matches any already placed ship
-			if (player1.ships.is_overlap(player1.ships.battleship) == True):
-				print "again?"
-			else:
-				#If no, update all ships dictionary
-				player1.ships.add_ship_loc_dict(player1.ships.battleship)
+# 				Print MSG bomb missed
 
-				#print player1.ships.all_ships
-				
-				#Update players ship_board
-				player1.my_ships_board.update_grid_ship(player1.ships.battleship)
-				
-				#Print players ship board
-				player1.print_my_ships_board()
+# 				Print updated enemy ship board
+
+# 				NEXT PLAYERS TURN
+
+# 			If TRUE
+
+# 				Save bomb location to bomb_list
+
+# 				Update enemy hit ship with HIT at bomb location
+
+# 				Print MSG that a ship was HIT
+
+# 				Update player enemy ship board with HIT
+
+# 				Update opponents  my ship board with HIT
+
+# 				Print updated enemy ship board
+
+# 				Check if opponents ship sunk
+
+# 				If FALSE
+
+# 					Print MSG no ship sunk
+
+# 					NEXT PLAYERS TURN
+
+# 				If TRUE
+
+# 					Print MSG that opponents ship sunk, Give name of sunk ship
+
+# 					Check if all of opponents ships sunk
+
+# 						If FALSE
+
+# 							NEXT PLAYERS TURN
+
+# 						If TRUE
+
+# 							Print MSG the all of opponents ships are sunk
+
+# 							Print MSG congrats MSG to winner
+
+# 							Print MSG that game is over
+
+# 							GAME OVER 
 
 
 
-	else:
-		#If no, update all ships dictionary
-		player1.ships.add_ship_loc_dict(player1.ships.battleship)
 
-		#print player1.ships.all_ships
-		
-		#Update players ship_board
-		player1.my_ships_board.update_grid_ship(player1.ships.battleship)
-		
-		#Print players ship board
-		player1.print_my_ships_board()
 
-# print player1.ship_tuple
-# print player1.ship_tuple[1]
-# print player1.ship_tuple[1][0]
-# START AGAIN WITH NEXT SHIP
 
 
 
