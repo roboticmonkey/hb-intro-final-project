00e8ff88b8a10bf147e7fb131ca93e1666bc23e6
aiden ward <aiden@Lagertha.local>
2015-10-13 22:35:00 -0700

    has function for placing ships

--- utilities.py
+++ utilities.py
@@ -55,63 +55,107 @@ def request_valid_location(raw_data, board):
 	
 	return raw_data
 
-# print request_start_location(test_grid)
+def request_placement_of_ship(ship_object, board_object, game_pieces_object, player_object):
+	while (ship_object.location_list_full() == False):
+		#Grid location list is empty. 
+		
+		print "Ships may only be placed horizontally or vertically."
 
-# name ="  fred  "
-# name2 = "fart knuckles"
-# name3 = "jim"
-# name4 = "djskah748"
+		#Ask user for start coordinate
+		print "Use letters A-J and numbers 1-10 for coordinates in the format 'A1'. "
+		print "Let's place your", ship_object.ship_name, "."
+		print ship_object.ship_name, "is ", ship_object.size, "cells long."
 
-# name = quick_fix(name)
-# print name, "is all alpha:", name.isalpha()
+		raw_data = raw_input("Please give the starting coordinates. Ex. 'A1'. ")
+		raw_data = quick_fix(raw_data)
+		# print raw_data
 
-# name2 = quick_fix(name2)
-# print name2, "is all alpha:", name2.isalpha()
+		#Validates string, asks for new one till it validates
+		temp_location_str = request_valid_location(raw_data, board_object)
 
-# name3 = quick_fix(name3)
-# print name3, "is all alpha:", name3.isalpha()
+			#Save start location string into ships start_location attribute
+		ship_object.ship_location_start(temp_location_str)
+			
+		# print ship_object.start_location
 
-# name4 = quick_fix(name4)
-# print name4, "is all alpha:", name4.isalpha()
+			#Convert start_location to a grid_location and save 
+		ship_object.create_grid_start_loc(board_object)
+		# print ship_object.grid_loc_start
 
-# test_value = "   f10   "
-# test_value2 = "r6" 
-# test_value3 = "  d14"
-# test_value4 = "   H0"
+		#Check of location is taken
+		#If all_ship dictionary is empty get end coordinates
+		if (game_pieces_object.all_ship_locations == {}):
+		#Ask for end location coordinates
+			print ship_object.start_location, "is your starting location. Ships may only be horizontal or vertical."
+			raw_data = raw_input("Enter your ending coordinates. ")
+			raw_data = quick_fix(raw_data)
 
-# print "test value 1"
-# print test_value
-# test_value = quick_fix(test_value)
-# print test_value
-# print "letter in range:", valid_letter(test_value, test_grid)
-# print "number in range:", valid_number(test_value)
+			#Valitdate user input string asks for valid string till it gets one. 
+			temp_location_str = request_valid_location(raw_data, board_object)
 
-# print "test test_value2"
-# print test_value2
-# test_value2 = quick_fix(test_value2)
-# print test_value2
-# print "letter in range:", valid_letter(test_value2, test_grid)
-# print "number in range:", valid_number(test_value2)
+			#Saves the location string
+			ship_object.ship_location_end(temp_location_str)	
 
-# print "test_value3"
-# print test_value3
-# test_value3 = quick_fix(test_value3)
-# print test_value3
-# print "letter in range:", valid_letter(test_value3, test_grid)
-# print "number in range." ,valid_number(test_value3)
+			#Save end location string into ship end_location attribute
+			ship_object.create_grid_end_loc(board_object)
 
-# print "test_value4"
-# print test_value4
-# test_value4 = quick_fix(test_value4)
-# print test_value4
-# print "letter in range:", valid_letter(test_value4, test_grid)
-# print "number in range:", valid_number(test_value4)
+			#Create ship grid location list
+			#If true, add ships grid_loc_list to game_pieces all_ships dict
+			ship_object.create_grid_loc_list()
+		else:
+		#Check if start grid location is taken
+		#Check if ship already at location
+			# print "dictionary not empty"
 
-		# def check_valid_end_location(self, end_location):
-		# if (self.start_location[0] == end_location[0]):
-		# 	if (self.start_location[1] - end_location[1] == self.size):
-		# 		return True 
-		# 	else:
-		# 		return False 
-		# elif (self.start_location[1] == end_location[1]):
-		# 	if ()
+			if (game_pieces_object.is_taken(ship_object.grid_loc_start) == False):
+			#Location not taken ask for end location
+
+			#Ask for end location coordinates
+				print ship_object.start_location, "is your starting location. Ships may only be horizontal or vertical."
+				raw_data = raw_input("Enter your ending coordinates. ")
+				raw_data = quick_fix(raw_data)
+
+				#Valitdate user input string asks for valid string till it gets one. 
+				temp_location_str = request_valid_location(raw_data, board_object)
+
+				#Saves the location string
+				ship_object.ship_location_end(temp_location_str)
+				#Creates the grid location
+				ship_object.create_grid_end_loc(board_object)
+						
+				#Check if end location is already taken
+				if (game_pieces_object.is_taken(ship_object.create_grid_end_loc) == True):
+					#If it is not taken
+					#Create ship grid location list
+					#DELETE all location variables
+					ship_object.erase_start_locations()
+					ship_object.erase_end_locations()
+					print "Ships maynot overlap. Please re-enter your coordinates."
+				else:
+					#Create ship grid location list
+					#If true, add ships grid_loc_list to game_pieces all_ships dict
+					ship_object.create_grid_loc_list()
+				#Check if ship location list overlaps a previous ship
+				if (game_pieces_object.is_overlap(ship_object) == True):
+					
+					#If it overlaps print error msg. 
+					print "Ships maynot overlap. Please re-enter your coordinates."
+					#DELETE all location variables
+					ship_object.erase_start_locations()
+					ship_object.erase_end_locations()
+					ship_object.erase_grid_location_list()
+					
+					#Go Back to getting Start Location coordinates
+
+	# print ship_object.grid_loc_list
+
+	# Save location list to ship location dictionary
+	game_pieces_object.add_ship_loc_dict(ship_object)
+
+
+	#Update players ship_board
+	board_object.update_grid_ship(ship_object)
+
+	#Print updated players ship board
+	player_object.print_my_ships_board()
+
